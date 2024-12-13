27b41a83123c8165afef8890c5f9e3dee583c754
Swapnapratim Bhaumik <swapnapratimbhaumik@PIL-AHD-LPT1061.local>
2024-12-13 20:15:36 +0530

    fixed type errors

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -2,54 +2,81 @@ const { BeaconProxy } = require("generated");
 
 // Handler for the `Deposit` event
 BeaconProxy.Deposit.handler(async ({ event, context }) => {
+  console.log("Received mock event:", event.params);
+  console.log("-----------------------");
+
+  const provider = event.params.provider; // Indexed: provider
+  const value = event.params._1.toString(); // Non-indexed: value
+  const locktime = event.params.locktime.toString(); // Indexed: locktime
+  const depositType = Number(event.params._3); // Non-indexed: type
+  const timestamp = event.params._4.toString(); // Non-indexed: ts
+
+  console.log({ provider, value, locktime, depositType, timestamp });
+
   const entity = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    provider: event.params.provider, // Address of the user
-    value: event.params.value,       // Amount deposited
-    locktime: event.params.locktime, // Lock time
-    depositType: event.params.type,  // Type of deposit (int128)
-    timestamp: event.params.ts,      // Timestamp of the event
+    provider,
+    value,
+    locktime,
+    depositType,
+    timestamp,
   };
 
   await context.BeaconProxy_Deposit.set(entity);
+  console.log("Deposit entity saved:", entity);
 });
 
 // Handler for the `Withdraw` event
 BeaconProxy.Withdraw.handler(async ({ event, context }) => {
+  console.log("Withdraw event received:", event.params);
+
+  const provider = event.params.provider; // Indexed: provider
+  const value = event.params._1.toString(); // Non-indexed: value
+  const timestamp = event.params._2.toString(); // Non-indexed: ts
+
   const entity = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    provider: event.params.provider, // Address of the user
-    value: event.params.value,       // Amount withdrawn
-    timestamp: event.params.ts,      // Timestamp of the withdrawal
+    provider,
+    value,
+    timestamp,
   };
 
   await context.BeaconProxy_Withdraw.set(entity);
+  console.log("Withdraw entity saved:", entity);
 });
 
 // Handler for the `WithdrawEarly` event
 BeaconProxy.WithdrawEarly.handler(async ({ event, context }) => {
+  console.log("WithdrawEarly event received:", event.params);
+
+  const provider = event.params.provider; // Indexed: provider
+  const penalty = event.params._1.toString(); // Non-indexed: penalty
+  const timeLeft = event.params._2.toString(); // Non-indexed: time left
+
   const entity = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    provider: event.params.provider, // Address of the user
-    penalty: event.params.penalty,   // Penalty applied
-    timeLeft: event.params.time_left, // Time left for lock expiration
+    provider,
+    penalty,
+    timeLeft,
   };
 
   await context.BeaconProxy_WithdrawEarly.set(entity);
+  console.log("WithdrawEarly entity saved:", entity);
 });
 
 // Handler for the `Supply` event
 BeaconProxy.Supply.handler(async ({ event, context }) => {
+  console.log("Supply event received:", event.params);
+
+  const previousSupply = event.params._0.toString(); // Non-indexed: prevSupply
+  const currentSupply = event.params._1.toString(); // Non-indexed: supply
+
   const entity = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    previousSupply: event.params.prevSupply, // Previous total supply
-    currentSupply: event.params.supply,     // New total supply
+    previousSupply,
+    currentSupply,
   };
 
   await context.BeaconProxy_Supply.set(entity);
+  console.log("Supply entity saved:", entity);
 });
-
-
-
-
-
--- test/Test.js
+++ test/Test.js
@@ -1,37 +1,91 @@
-
 const assert = require("assert");
 const { TestHelpers } = require("generated");
 const { MockDb, BeaconProxy } = TestHelpers;
 
-describe("BeaconProxy contract AcceptEarlyUnlockOwnership event tests", () => {
-  // Create mock db
-  const mockDb = MockDb.createMockDb();
+describe("BeaconProxy Event Handlers", () => {
+  it("Processes a Deposit event correctly", async () => {
+    const mockDbInitial = MockDb.createMockDb();
 
-  // Creating mock for BeaconProxy contract AcceptEarlyUnlockOwnership event
-  const event = BeaconProxy.AcceptEarlyUnlockOwnership.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+    // Manually construct a mock event
+    const mockDepositEvent = {
+      params: {
+        provider: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        _1: BigInt("1000000000000000000000"), // value
+        locktime: BigInt("1766016000"), // locktime
+        _3: BigInt(1), // depositType
+        _4: BigInt("1734097068"), // timestamp
+      },
+      chainId: 11155111,
+      block: {
+        number: 123456,
+        timestamp: 1734097068, // Correct type
+      },
+      transaction: {
+        hash: "0xmocktransactionhash",
+      },
+      logIndex: 0,
+    };
 
-  it("BeaconProxy_AcceptEarlyUnlockOwnership is created correctly", async () => {
-    // Processing the event
-    const mockDbUpdated = await BeaconProxy.AcceptEarlyUnlockOwnership.processEvent({
-      event,
-      mockDb,
+    // Pass the mock event directly to the handler
+    const updatedMockDb = await BeaconProxy.Deposit.processEvent({
+      event: mockDepositEvent,
+      mockDb: mockDbInitial,
     });
 
-    // Getting the actual entity from the mock database
-    let actualBeaconProxyAcceptEarlyUnlockOwnership = mockDbUpdated.entities.BeaconProxy_AcceptEarlyUnlockOwnership.get(
-      `${event.chainId}_${event.block.number}_${event.logIndex}`
+    const expectedDepositEntity = {
+      id: "11155111_123456_0",
+      provider: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+      value: "1000000000000000000000",
+      locktime: "1766016000",
+      depositType: 1,
+      timestamp: "1734097068",
+    };
+
+    const actualDepositEntity = updatedMockDb.entities.BeaconProxy_Deposit.get(
+      expectedDepositEntity.id
     );
 
-    // Creating the expected entity
-    const expectedBeaconProxyAcceptEarlyUnlockOwnership = {
-      id:`${event.chainId}_${event.block.number}_${event.logIndex}`,
-      admin: event.params.admin,
+    assert.deepStrictEqual(actualDepositEntity, expectedDepositEntity);
+  });
+
+  it("Processes a Withdraw event correctly", async () => {
+    const mockDbInitial = MockDb.createMockDb();
+
+    // Manually construct a mock event
+    const mockWithdrawEvent = {
+      params: {
+        provider: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        _1: BigInt("500000000000000000000"), // value
+        _2: BigInt("1734098068"), // timestamp
+      },
+      chainId: 11155111,
+      block: {
+        number: 123457,
+        timestamp: 1734098068,
+      },
+      transaction: {
+        hash: "0xmocktransactionhash2",
+      },
+      logIndex: 0,
     };
-    // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(
-      actualBeaconProxyAcceptEarlyUnlockOwnership,
-      expectedBeaconProxyAcceptEarlyUnlockOwnership,
-      "Actual BeaconProxyAcceptEarlyUnlockOwnership should be the same as the expectedBeaconProxyAcceptEarlyUnlockOwnership"
+
+    // Pass the mock event directly to the handler
+    const updatedMockDb = await BeaconProxy.Withdraw.processEvent({
+      event: mockWithdrawEvent,
+      mockDb: mockDbInitial,
+    });
+
+    const expectedWithdrawEntity = {
+      id: "11155111_123457_0",
+      provider: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+      value: "500000000000000000000",
+      timestamp: "1734098068",
+    };
+
+    const actualWithdrawEntity = updatedMockDb.entities.BeaconProxy_Withdraw.get(
+      expectedWithdrawEntity.id
     );
+
+    assert.deepStrictEqual(actualWithdrawEntity, expectedWithdrawEntity);
   });
 });
